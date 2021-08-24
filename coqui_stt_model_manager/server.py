d32ad5f4917fe66c13ab14fdef2641793f67b7a0
Reuben Morais <reuben.morais@gmail.com>
2021-08-24 10:03:45 +0200

    Revert debugging changes

--- coqui_stt_model_manager/server.py
+++ coqui_stt_model_manager/server.py
@@ -23,7 +23,7 @@ from .modelmanager import ModelCard, ModelManager
 
 Payload.max_decode_packets = 10000
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:8000", "https://coqui.ai"])
+CORS(app, origins=["https://coqui.ai"])
 socketio = SocketIO(app)
 
 VAD = Vad(3)  # Very aggressive
@@ -64,7 +64,7 @@ def index():
     print(f"Current installs: {current_installs}")
     return render_template(
         "index.html",
-        model_zoo_url=f"http://localhost:8000/models?callback_url=http://{host}:{port}/install_model&prefer_tflite=1",
+        model_zoo_url=f"https://coqui.ai/models?callback_url=http://{host}:{port}/install_model&prefer_tflite=1",
         installed_models=list(app.config["MODEL_MANAGER"].list_models()),
         models_being_installed=current_installs,
     )
