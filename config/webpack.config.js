44209fd06131da6563a20d903d7960b9016b36df
wasertech <danny@waser.tech>
2022-07-13 11:52:45 +0200

    fix chunkLoadingGlobal

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -181,7 +181,7 @@ module.exports = function(webpackEnv) {
           (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
       // Prevents conflicts when multiple webpack runtimes (from different apps)
       // are used on the same page.
-      jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+      chunkLoadingGlobal: `webpackJsonp${appPackageJson.name}`,
       // this defaults to 'window', but by setting it to 'this' then
       // module chunks which are built will work in web workers as well.
       globalObject: 'this',
