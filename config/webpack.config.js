622979bd7c89611c511caa00e11381ab31a46652
wasertech <danny@waser.tech>
2022-07-13 11:53:38 +0200

    fix futureEmitAssets

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -165,8 +165,6 @@ module.exports = function(webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: 'static/js/bundle.min.js',
-      // TODO: remove this when upgrading to webpack 5
-      futureEmitAssets: true,
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
